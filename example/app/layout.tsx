7d0a23cc9bff5fafdec3b628d0f7668f919f8fe9
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-07-01 17:26:16 +0200

    add example stuff

--- example/app/layout.tsx
+++ example/app/layout.tsx
@@ -1,14 +1,15 @@
 import './globals.css'
-import { Inter as FontSans } from 'next/font/google'
+import { Space_Mono } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
 import { ReactNode } from 'react'
 import { Toaster } from 'sonner'
 import AppLayout from '@/components/AppLayout'
 
-const fontSans = FontSans({
+const inter = Space_Mono({
   subsets: ['latin'],
-  variable: '--font-sans',
+  weight: ['400', '700'],
+  style: 'normal',
 })
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -18,7 +19,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={cn(
           'min-h-screen bg-background flex flex-col items-center justify-center font-sans antialiased',
-          fontSans.variable
+          inter.className
         )}
       >
         {children}
--- example/components/ui/badge.tsx
+++ example/components/ui/badge.tsx
@@ -16,7 +16,7 @@ const badgeVariants = cva(
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
         success:
-          'border-transparent bg-success text-success-foreground hover:bg-success/80',
+          'border-transparent bg-green-500  text-primary-foreground hover:bg-success/80',
       },
     },
     defaultVariants: {
