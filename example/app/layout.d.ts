ac6f0f69c2b371029ae8ba1a3545ad89950fb587
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-08-22 11:33:57 -0700

    rm built files

--- example/app/layout.d.ts
+++
@@ -1,6 +0,0 @@
-import './globals.css';
-import { ReactNode } from 'react';
-export default function RootLayout({ children }: {
-    children: ReactNode;
-}): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=layout.d.ts.map
--- example/app/page.d.ts
+++
@@ -1,2 +0,0 @@
-export default function Home(): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=page.d.ts.map
